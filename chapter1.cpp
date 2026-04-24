bf2080161f52bb06c9343ff52e0e06eefdd16627
Malaika Shoukat <shoukatmalaika11@gmail.com>
2026-04-24 10:29:01 +0500

    Initial commit - DSA Course in C++

---
+++ chapter1.cpp
@@ -0,0 +1,18 @@
+// Variables DataTypes and operators 
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Wellcome to the DSA series from CodeBYMalaika"<<endl;
+    // variables -----containers to store datatypes
+    int a= 10;
+    cout<< a<< "\n";
+//    Datatypes ---- int, char, float, bool, double 
+int b= 15;
+char grade='A';
+float marks=80.5;
+bool isNum= true;
+double exactPrice= 999.99;
+cout<< exactPrice << "\n";
+
+   
+}
