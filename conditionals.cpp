af0da131c301b6e9032481669bb48159645bd7c4
Malaika Shoukat <shoukatmalaika11@gmail.com>
2026-04-28 09:51:51 +0500

    conditional statements

---
+++ conditionals.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // eligibility for vote 
+    // int age;
+    // cout<< "Enter your age:";
+    // cin >> age;
+    // if (age>=18){
+    //     cout << "your are eligible to vote";
+
+    // }else{
+    //     cout << "your are not eligible to vote";
+    // }
+    // // check even and odd numbers 
+    // int num;
+    // cout << "enter a number:";
+    // cin >> num;
+    // if(num%2==0){
+    //     cout<<num<<" is even number";
+
+    // }else {
+    //     cout<<num<<" is odd number";
+    // }
+
+    // grading system 
+    // int marks;
+    // cout << "enter your marks:";
+    // cin >> marks;
+    // if (marks >= 90){
+    //   cout <<"A+";
+    // }else if (marks>=80){
+    //     cout<< "A";
+    // }else if(marks>=70){
+    //     cout<< "B";
+
+    // }else if (marks>=60){
+    //     cout << "C";
+
+    // }else{
+    //     cout << "F";
+    // }
+
+    // checking the charater wheather is uppercase or lowercase 
+char ch;
+cout <<" Enter the char";
+cin >> ch;
+if(ch>='a' && ch<='z'){
+    cout << ch << "is the lowercase letter";
+}else{
+    cout << ch << "is the uppercase letter";
+    
+}
+
+}
