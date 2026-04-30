906ecd42716d89095bf289a914e9bacc4080d355
Malaika Shoukat <shoukatmalaika11@gmail.com>
2026-04-30 22:12:19 +0500

    ternary.cpp

---
+++ ternary.cpp
@@ -0,0 +1,9 @@
+// Ternary Statement in C++ 
+#include <iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Enter your marks:";
+    cin>>marks;
+    marks>=50? "passed":"failed";
+}
