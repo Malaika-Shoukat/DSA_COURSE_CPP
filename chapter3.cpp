a27e89a16296e20ed84a116e45edbce37b785b17
Malaika Shoukat <shoukatmalaika11@gmail.com>
2026-05-02 17:30:41 +0500

    loops

---
+++ chapter3.cpp
@@ -0,0 +1,24 @@
+// loops 
+#include <iostream>
+using namespace std;
+int main(){
+    // loops are used to repeat a block of code  multiple times until a certain condition is met.
+    // 3 types of loops in c++: while loop, do while loop, for loop 
+    // while loop
+    // int count = 0;
+    // while (count<=5){
+    //     cout<<count<<"\n";
+    //     count ++;
+    return 0;
+    // }
+    // do while loop 
+   
+    // for loop 
+
+    for (int number = 0; number <= 5; number++)
+    {
+        cout << number << "\n";
+    }
+
+    return 0;
+}
